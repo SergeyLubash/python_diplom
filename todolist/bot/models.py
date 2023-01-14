715df4a6f56c4b03fba2e12638aeac6a7df2fec4
SergeyLubash <luba-sergei@yandex.ru>
2023-01-14 09:28:52 +0300

    Add all

--- todolist/bot/models.py
+++ todolist/bot/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from django.core.validators import MinValueValidator
+from django.core.validators import MinLengthValidator
 from django.utils.crypto import get_random_string
 
 
 class TgUser(models.Model):
     tg_chat_id = models.BigIntegerField()
     tg_user_id = models.BigIntegerField(unique=True)
-    tg_username = models.CharField(max_length=32, validators=[MinValueValidator(5)])
+    tg_username = models.CharField(max_length=32, validators=[MinLengthValidator(5)])
     user = models.ForeignKey('core.User', null=True, on_delete=models.CASCADE)
     verification_code = models.CharField(max_length=10, unique=True)
 
